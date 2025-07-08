e5a47f417919fcbed33b3292ed28548c56ddd75b
BhanuReddy700 <nbhanuprakashreddy25@gmail.com>
2025-07-08 18:37:46 +0530

    added all three functions

--- this_keyword.js
+++ this_keyword.js
@@ -11,3 +11,29 @@ function outer() {
 const increment = outer();
 console.log(increment()); 
 console.log(increment()); 
+
+
+function abc(){
+  console.log(`hi im ${this.name}`)
+}
+
+const me = {
+  name:"bhanu"
+}
+const me2 = {
+  name:"saini"
+}
+const k = abc.bind(me)
+const l = abc.bind(me2)
+console.log(k())
+l()
+
+const an = (a,b) => a+b
+
+console.log(an(3,5))
+
+const man = function(a,b){
+  return a+b
+}
+
+console.log(man(7,7))
