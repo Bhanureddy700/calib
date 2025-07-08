55288467264a8008677eafb81d3b12a14b0f0dd7
BhanuReddy700 <nbhanuprakashreddy25@gmail.com>
2025-07-08 10:08:59 +0530

    Add all_functions.js

---
+++ all_functions.js
@@ -0,0 +1,16 @@
+const fun = function(a,b){
+    return a+b
+}
+console.log(fun(5,9))
+
+
+const fun1 =()=>{
+    console.log("hello my name is bhanu");
+}
+console.log(fun1())
+
+const fuun2 = (c,d)=>{
+    console.log(c+d)
+    return c+d
+}
+console.log(fuun2())
