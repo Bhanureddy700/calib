655d8077b8c91295dcb4d0664fa9bc216b00033a
BhanuReddy700 <nbhanuprakashreddy25@gmail.com>
2025-07-09 10:09:03 +0530

    added class_object

---
+++ class_objects.js
@@ -0,0 +1,20 @@
+class person{
+    constructor(name,age){
+        this.name = name
+        this.age = age
+    }
+
+    wishing(){
+        console.log(`happy birth ${this.name} on your ${this.age}th birthday`)
+    }
+    abc(){
+        this.age+=1
+        console.log(`just today he turned ${this.age}`)
+    }
+}
+
+const p1 = new person("bhanu",20)
+const p2 = new person("sai",21)
+
+p1.wishing();
+p1.abc();
