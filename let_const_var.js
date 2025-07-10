1ec2a03d5c837236d24ab38244f9d6498f0d77b9
BhanuReddy700 <nbhanuprakashreddy25@gmail.com>
2025-07-10 13:52:42 +0530

    jai

--- let_const_var.js
+++ let_const_var.js
@@ -1,5 +1,10 @@
 let a = 0;
 a = 1;
+var k = 9
+
+const abc = {
+    name:console.log(k)
+}
 console.log(a)
 var b = 10
 var b = 15
@@ -7,3 +12,18 @@ console.log(b)
 const c = 9
 
 console.log(c)
+
+
+let ab = "monday"
+
+switch(ab){
+    case "monday":
+        console.log("today is monday")
+        break
+    case "tuesday":
+        console.log("today is tues")
+        break
+    case "wednesday":
+        console.log("today is wednes")
+        break
+}
